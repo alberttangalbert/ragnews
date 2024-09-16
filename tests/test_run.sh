0d7334af7d7dce34044fca7f03619e3754b1ddeb
albert-bpc <atang@beachpointcapital.com>
2024-09-15 22:24:16 -0700

    add test.yml and add tests folder with sh test cases

---
+++ tests/test_run.sh
@@ -0,0 +1,23 @@
+# Test cases for ragnews.db
+queries_ragnews=(
+  "tell me about the current presidential candidates"
+  "who is projected to win the 2024 election?"
+  "who is donald trump"
+  "what are donald trump's policies?"
+)
+
+for query in "${queries_ragnews[@]}"; do
+  echo "$query" | python3 ../ragnews/run.py --db="../ragnews/sql_dbs/ragnews.db"
+done
+
+# Test cases for claremont.db
+queries_claremont=(
+  "tell me about the claremont colleges"
+  "which claremont college is the best for economics?"
+  "what is the advantage of going to claremont mckenna over the other colleges?"
+  "which claremont college has the highest tuition?"
+)
+
+for query in "${queries_claremont[@]}"; do
+  echo "$query" | python3 ../ragnews/run.py --db="../ragnews/sql_dbs/claremont.db"
+done
---
+++ tests/update_db-claremont.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+urls='
+https://tsl.news/
+https://www.claremontindependent.com/
+https://www.cmcforum.com/
+https://www.scrippscollege.edu/news/tag/artificial-intelligence
+https://www.pomona.edu/news
+https://www.hmc.edu/about/news/
+https://www.kgi.edu/about/branding-news-pr/news/
+https://www.cmc.edu/newsfeed
+https://www.pitzer.edu/news
+https://www.cgu.edu/news/
+'
+for url in $urls; do
+    echo url
+    python3 ../ragnews/run.py --add_url="$url" --recursive_depth=0 --db=../ragnews/sql_dbs/ragnews.db --loglevel=INFO
+done
